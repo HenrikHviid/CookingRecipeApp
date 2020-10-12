6b3fec3e358b53390fa5099f21c57cedd2bc1341
Danny Nguyen <dannyhoai@hotmail.com>
2020-10-12 12:37:11 +0200

    new cache strategy in sw.js

--- sw.js
+++ sw.js
@@ -30,12 +30,28 @@ self.addEventListener('activate', function (e) {
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return fetch(e.request) || response;
+// Cache, falling back to network
+// self.addEventListener('fetch', function (e) {
+//   //console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then(function (response) {
+//       return response || fetch(e.request) ;
+//     })
+//   );
+// });
+
+// Cache, falling back to network with frequent updates
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.open(cacheName).then(function (cache) {
+      return cache.match(e.request).then(function (cachedResponse) {
+        var fetchPromise =
+          fetch(e.request).then(function (networkResponse) {
+            cache.put(e.request, networkResponse.clone());
+            return networkResponse;
+          });
+        return cachedResponse || fetchPromise;
       })
-    );
-  }); 
-  
+    })
+  );
+});
